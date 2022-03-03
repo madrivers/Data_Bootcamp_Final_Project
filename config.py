cbe037761719d8a4d7c8068005fda2715e408119
rafapeterson <rafaaapeterson@gmail.com>
2022-03-02 17:48:48 -0700

    uploading archived files

---
+++ config.py
@@ -0,0 +1,9 @@
+databases = {
+    'production':{
+        'NAME': 'group_project_1',
+        'USER': 'postgres',
+        'PASSWORD': 'Gpbc2021',
+        'HOST': 'group-project.cwzrivsabdh9.us-east-1.rds.amazonaws.com',
+        'PORT': 5432,
+    },
+}
